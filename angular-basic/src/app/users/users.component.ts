67114b6558aed74c966ca413922dec3d017c66a9
Abhijeet Shende <GS-3467@GSLAB.COM>
2023-01-15 19:12:15 +0530

    event binding

--- angular-basic/src/app/users/users.component.ts
+++ angular-basic/src/app/users/users.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-users',     // [] -> attribute, .app-user -> as a class,  as element -> <app-user>, #app-user as id selector
+  selector: 'app-users', // [] -> attribute, .app-user -> as a class,  as element -> <app-user>, #app-user as id selector
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent {
- name = "abhijeet";
- private age =34;
- allowNewUser :boolean = true;
- constructor(){
-  setTimeout(()=>{
-    this.allowNewUser = false;
-  },3000)
- }
- getage (){
-  return this.age;
- }
+  name = 'abhijeet';
+  private age = 34;
+  allowNewUser: boolean = true;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewUser = false;
+    }, 3000);
+  }
+  getage() {
+    return this.age;
+  }
+
+  eventBinded_string: string = 'this is a default string';
+  changeStringUsingEvent() {
+    setTimeout(() => {
+      this.eventBinded_string = 'string  has been changed';
+    }, 1000);
+  }
 }
